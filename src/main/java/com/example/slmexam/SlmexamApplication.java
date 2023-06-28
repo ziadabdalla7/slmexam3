c634e23a8c2f14ab18c0322dd90e09cbda01bd80
Z <wi22b001>
2023-06-28 18:45:41 +0200

    Initial commit

---
+++ src/main/java/com/example/slmexam/SlmexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slmexam/SlmexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
